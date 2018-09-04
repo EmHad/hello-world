9544edc3c04cd2cb9aebc7602f76f604b8a75e3e
Emir Hadziosmanovic <emhad@hotmail.com>
2018-09-04 13:49:33 +0200

    Updatering från VS

--- GitDemo2/GitDemo2/Program.cs
+++ GitDemo2/GitDemo2/Program.cs
@@ -13,6 +13,7 @@ namespace GitDemo2
             String s = "New version 2";
             String t = "Master version";
             String k = "En till update";
+            String l = "Updatering från VS";
         }
     }
 }
